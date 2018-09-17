57929c5cdd4492f58333791df1b9cbff8d7c9e19
rzevel <richard.zevel@gmail.com>
2018-09-17 17:53:16 +0200

    rollback to previous version - working

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-import NodeList from './nodeList'
+class Node {
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
+}
+
+class NodeList {
+  constructor() {
+    this.head = null
+    this.length = 0
+  }
+
+  addToList(data) {
+    let nodeToBeAdded = new Node(data)
+    let checkLastNode = this.head
+
+    if (!checkLastNode) {
+      this.head = nodeToBeAdded
+      this.length++
+      return nodeToBeAdded
+    }
+
+    while (checkLastNode.next) {
+      checkLastNode = checkLastNode.next
+    }
+
+    checkLastNode.next = nodeToBeAdded
+    this.length++
+    return nodeToBeAdded
+  }
+}
 
 let list = new NodeList()
 for (let i = 0; i < 5; i++) {
